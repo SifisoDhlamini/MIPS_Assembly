1ac48516a8849a707086afb7bad9d0686903eae1
Scifi_the_dev <sifisoldhlamini@outlook.com>
2022-04-05 22:17:49 +0800

    nested functions program structure

--- register2stack.asm
+++ register2stack.asm
@@ -14,9 +14,7 @@
 		syscall
 		
 		#print value
-		li $v0, 1
-		move $a0, $s0
-		syscall
+		jal printValue
 	
 	# Tell the system that the program is done
 	li $v0, 10
@@ -24,18 +22,25 @@
 	
 	#callee procedure
 	increaseMyRegister:
-		addi $sp, $sp, -4 #reserve 4 bytes in stack to pointer
+		addi $sp, $sp, -8 #reserve 8 bytes in stack to pointer (2 addresses
 		sw $s0, 0($sp) # store $so in the first space allocated in the stack
+		sw $ra, 4($sp) # store return address
 		
 		addi $s0, $s0, 30
 		
-		# print new value in function
+		# print new value in function (nested procedure)
+		jal printValue
+		
+		lw $s0, 0($sp) #restore value of $s0
+		lw $ra, 4($sp) #give return address on the stack
+		addi $sp, $sp, 4 # restore pointer to original position
+		
+		jr $ra # return to main
+	
+	printValue:
 		li $v0, 1
 		move $a0, $s0
 		syscall
 		
-		lw $s0, 0($sp) #restore value of $s0
-		addi $sp, $sp, 4 # restore pointer to original position
-		
-		jr $ra # return to main
+		jr $ra
 		
